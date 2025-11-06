07769f309aa522c0b0d2711eca67525bf5650e2e
Paweł Zatoka <github@hoxi.me>
2025-11-06 06:35:18 +0100

    Adjust completion menu config

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -32,6 +32,7 @@ return {
         opts = {},
       },
       'folke/lazydev.nvim',
+      'onsails/lspkind.nvim',
     },
     --- @module 'blink.cmp'
     --- @type blink.cmp.Config
@@ -53,6 +54,35 @@ return {
         -- By default, you may press `<c-space>` to show the documentation.
         -- Optionally, set `auto_show = true` to show the documentation after a delay.
         documentation = { auto_show = true, auto_show_delay_ms = 500 },
+        menu = {
+          draw = {
+            components = {
+              kind_icon = {
+                ellipsis = false,
+                text = function(ctx)
+                  local icon = ctx.kind_icon
+
+                  if vim.tbl_contains({ 'Path' }, ctx.source_name) then
+                    local dev_icon, _ = require('nvim-web-devicons').get_icon(ctx.label)
+                    if dev_icon then
+                      icon = dev_icon
+                    end
+                  else
+                    icon = require('lspkind').symbolic(ctx.kind, {
+                      mode = 'symbol',
+                    })
+                  end
+
+                  return icon .. ctx.icon_gap
+                end,
+              },
+            },
+            columns = {
+              { 'label', 'label_description', gap = 1 },
+              { 'kind_icon', 'kind', 'source_name' },
+            },
+          },
+        },
       },
 
       sources = {
