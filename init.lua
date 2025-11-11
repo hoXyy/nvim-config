5cb7d2da019a70e7541cd67d3d4b9b095a1f24c3
Paweł Zatoka <github@hoxi.me>
2025-11-11 23:54:12 +0100

    Fix error when deleting files through neo-tree

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- Highlight when yanking (copying) text
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
   callback = function()
     vim.hl.on_yank()
   end,
