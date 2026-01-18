35bcc873015fdb67926e5dfc9a55369d5b4b3a4b
Paweł Zatoka <github@hoxi.me>
2026-01-18 18:30:00 +0100

    Add auto word highlight to LSP settings

--- lua/plugins/languages/lsp.lua
+++ lua/plugins/languages/lsp.lua
@@ -7,7 +7,6 @@ local function any_client_supports(bufnr, capability)
   return false
 end
 
-
 -- server configs
 local vue_plugin = {
   name = '@vue/typescript-plugin',
@@ -111,6 +110,39 @@ vim.diagnostic.config {
     end,
   },
 }
+
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('lsp-attach', { clear = false }),
+  callback = function(event)
+    -- The following two autocommands are used to highlight references of the
+    -- word under your cursor when your cursor rests there for a little while.
+    --    See `:help CursorHold` for information about when this is executed
+    --
+    -- When you move your cursor, the highlights will be cleared (the second autocommand).
+    if any_client_supports(event.buf, 'documentHighlightProvider') then
+      local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
+      vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.clear_references,
+      })
+
+      vim.api.nvim_create_autocmd('LspDetach', {
+        group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
+        callback = function(event2)
+          vim.lsp.buf.clear_references()
+          vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
+        end,
+      })
+    end
+  end,
+})
 
 for server, config in pairs(servers) do
   vim.lsp.config(server, config)
