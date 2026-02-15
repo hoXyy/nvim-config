1cb68aa495fa3e9ecc582157ae0c4cf0b11c0239
Paweł Zatoka <github@hoxi.me>
2026-02-15 17:41:21 +0100

    Automatically install vtsls
    oops

--- lua/plugins/languages/mason.lua
+++ lua/plugins/languages/mason.lua
@@ -2,5 +2,5 @@ require('mason').setup()
 require('mason-lspconfig').setup { automatic_enable = false }
 
 require('mason-tool-installer').setup {
-  ensure_installed = { 'lua_ls', 'bashls', 'jsonls', 'html', 'eslint', 'vue_ls', 'cssls', 'astro', 'stylua', 'yamlls', 'gh_actions_ls' },
+  ensure_installed = { 'lua_ls', 'bashls', 'jsonls', 'html', 'eslint', 'vue_ls', 'cssls', 'astro', 'stylua', 'yamlls', 'gh_actions_ls', 'vtsls' },
 }
