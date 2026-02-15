09573cf604fdf37d3e1e4ef1dbdf65d6570a31d6
Paweł Zatoka <github@hoxi.me>
2026-02-15 18:17:40 +0100

    Add npm test command, update npm install command to use Snacks floating terminal

--- lua/commands.lua
+++ lua/commands.lua
@@ -27,5 +27,35 @@ vim.api.nvim_create_user_command('NpmInstall', function()
     pnpm = 'pnpm install',
     bun = 'bun install',
   }
-  vim.cmd('!' .. install_cmd[packageManager])
+
+  Snacks.terminal.open(install_cmd[packageManager], {
+    win = {
+      style = 'terminal',
+    },
+    auto_close = false,
+  })
 end, { desc = 'install npm dependencies, with automatic package manager detection' })
+
+-- npm test, with automatic package manager detection
+vim.api.nvim_create_user_command('NpmTest', function()
+  local isNodeProject = require('helpers.npm-helpers').isNodeProject()
+  if not isNodeProject then
+    vim.notify('Not in Node.js project, aborting...', vim.log.levels.WARN)
+    return
+  end
+
+  local packageManager = require('helpers.npm-helpers').detectPackageManager()
+  local test_cmd = {
+    npm = 'npm test',
+    yarn = 'yarn test',
+    pnpm = 'pnpm test',
+    bun = 'bun test',
+  }
+
+  Snacks.terminal.open(test_cmd[packageManager], {
+    win = {
+      style = 'terminal',
+    },
+    auto_close = false,
+  })
+end, { desc = 'run npm tests, with automatic package manager detection' })
