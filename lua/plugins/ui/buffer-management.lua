799c2b94de5b2f9e2989f401ff6d1c099be261d4
Paweł Zatoka <github@hoxi.me>
2026-02-18 20:53:29 +0100

    Replace barbar with bufferline

--- lua/plugins/ui/buffer-management.lua
+++ lua/plugins/ui/buffer-management.lua
@@ -1,33 +1,47 @@
 return {
   {
-    'romgrk/barbar.nvim',
-    init = function()
-      vim.g.barbar_auto_setup = false
+    'akinsho/bufferline.nvim',
+    config = function()
+      require('bufferline').setup {
+        options = {
+          style_preset = require('bufferline').style_preset.no_italic,
+          truncate_names = false,
+          offsets = {
+            {
+              filetype = 'NvimTree',
+              separator = true,
+            },
+          },
+        },
+      }
     end,
-    opts = {},
     lazy = false,
+    version = '*',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
     keys = {
       {
         '[b',
-        '<cmd>BufferPrevious<CR>',
+        '<cmd>BufferLineCyclePrev<CR>',
         mode = 'n',
         desc = 'Previous buffer',
       },
       {
         ']b',
-        '<cmd>BufferNext<CR>',
+        '<cmd>BufferLineCycleNext<CR>',
         mode = 'n',
         desc = 'Next buffer',
       },
-      {
-        '<leader>bc',
-        '<cmd>BufferClose<CR>',
-        mode = 'n',
-        desc = 'Close buffer',
-      },
+      -- {
+      --   '<leader>bc',
+      --   '<cmd>BufferClose<CR>',
+      --   mode = 'n',
+      --   desc = 'Close buffer',
+      -- },
       {
         '<leader>bp',
-        '<cmd>BufferPin<CR>',
+        '<cmd>BufferLineTogglePin<CR>',
         mode = 'n',
         desc = 'Pin buffer',
       },
