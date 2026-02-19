7a70e0d6c3718ecd6cf2a5fed5d9997197b85ac1
Paweł Zatoka <github@hoxi.me>
2026-02-19 21:13:26 +0100

    theme adjust

--- lua/plugins/ui/colorscheme.lua
+++ lua/plugins/ui/colorscheme.lua
@@ -4,6 +4,16 @@ return {
     priority = 100,
     lazy = false,
     config = function()
+      require('tokyonight').setup {
+        style = 'storm',
+        styles = {
+          comments = { italic = false },
+          functions = { italic = false },
+          keywords = { italic = false },
+        },
+        lualine_bold = true,
+      }
+
       vim.cmd.colorscheme 'tokyonight'
     end,
   },
