ceffa78ebc127aa92f1f1f9b97498bef4d0f452c
Paweł Zatoka <github@hoxi.me>
2026-02-19 20:21:25 +0100

    disable icons in diagnostics virtual text

--- lua/plugins/languages/lsp.lua
+++ lua/plugins/languages/lsp.lua
@@ -248,15 +248,15 @@ return {
           source = 'if_many',
           spacing = 2,
           border = 'rounded',
-          format = function(diagnostic)
-            local diagnostic_message = {
-              [vim.diagnostic.severity.ERROR] = ' ' .. diagnostic.message,
-              [vim.diagnostic.severity.WARN] = ' ' .. diagnostic.message,
-              [vim.diagnostic.severity.INFO] = ' ' .. diagnostic.message,
-              [vim.diagnostic.severity.HINT] = '󰌵 ' .. diagnostic.message,
-            }
-            return diagnostic_message[diagnostic.severity]
-          end,
+          -- format = function(diagnostic)
+          --   local diagnostic_message = {
+          --     [vim.diagnostic.severity.ERROR] = ' ' .. diagnostic.message,
+          --     [vim.diagnostic.severity.WARN] = ' ' .. diagnostic.message,
+          --     [vim.diagnostic.severity.INFO] = ' ' .. diagnostic.message,
+          --     [vim.diagnostic.severity.HINT] = '󰌵 ' .. diagnostic.message,
+          --   }
+          --   return diagnostic_message[diagnostic.severity]
+          -- end,
         },
         jump = { float = true, wrap = true },
       }
