8bf7f536b1a53d8587f71f1b6854f23870140db8
Paweł Zatoka <github@hoxi.me>
2026-02-25 10:26:11 +0100

    add prettier formatting to conform.nvim

--- lua/plugins/languages/formatting.lua
+++ lua/plugins/languages/formatting.lua
@@ -17,11 +17,36 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         nix = { 'alejandra' },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescriptreact = { 'prettier' },
       },
       notify_on_error = false,
       format_on_save = function(bufnr)
         local disable_filetypes = { c = true, cpp = true }
-        if disable_filetypes[vim.bo[bufnr].filetype] then
+
+        local function has_prettier_eslint_plugin()
+          local cwd = vim.fn.getcwd()
+
+          -- npm/yarn: direct node_modules entry
+          if vim.fn.isdirectory(cwd .. '/node_modules/eslint-plugin-prettier') == 1 then
+            return true
+          end
+
+          -- pnpm: virtual store (.pnpm/eslint-plugin-prettier@<version>/...)
+          local pnpm_store = cwd .. '/node_modules/.pnpm'
+          if vim.fn.isdirectory(pnpm_store) == 1 then
+            local matches = vim.fn.glob(pnpm_store .. '/eslint-plugin-prettier@*', false, true)
+            if #matches > 0 then
+              return true
+            end
+          end
+
+          return false
+        end
+
+        if disable_filetypes[vim.bo[bufnr].filetype] or has_prettier_eslint_plugin() then
           return nil
         else
           return {
