8ebc08823d945400521f87038a572585e284c5c0
Paweł Zatoka <github@hoxi.me>
2026-05-14 20:29:11 +0200

    zig
    also bumps plugins

--- lua/plugins/languages/lsp.lua
+++ lua/plugins/languages/lsp.lua
@@ -13,6 +13,36 @@ return {
       },
     },
   },
+  {
+    url = 'https://codeberg.org/ziglang/zig.vim',
+    ft = { 'zig', 'zon' },
+    config = function()
+      vim.g.zig_fmt_parse_errors = 0
+      vim.g.zig_fmt_autosave = 0
+
+      vim.api.nvim_create_autocmd('BufWritePre', {
+        pattern = { '*.zig', '*.zon' },
+        callback = function()
+          vim.lsp.buf.format()
+
+          vim.lsp.buf.code_action {
+            ---@diagnostic disable-next-line: missing-fields
+            context = { only = { 'source.fixAll' } },
+            apply = true,
+          }
+        end,
+      })
+
+      vim.lsp.config['zls'] = {
+        cmd = { 'zls' },
+        filetypes = { 'zig', 'zon' },
+        root_markers = { 'build.zig' },
+        settings = { zls = {} },
+      }
+
+      vim.lsp.enable 'zls'
+    end,
+  },
   -- TODO: decide if i want to keep this
   {
     'esmuellert/nvim-eslint',
